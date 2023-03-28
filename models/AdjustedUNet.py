78d15146deb63bd7cbfb08b8d664d7b94d4e8ef4
Pawel Skiers <pawelsk269@gmail.com>
2023-03-29 00:03:29 +0200

    Add AdjustedUNet model

---
+++ models/AdjustedUNet.py
@@ -0,0 +1,42 @@
+import torch as th
+from ldm.modules.diffusionmodules.openaimodel import UNetModel
+from ldm.modules.diffusionmodules.util import timestep_embedding
+
+
+class AdjustedUNet(UNetModel):
+    def forward(self, x, timesteps=None, context=None, y=None, **kwargs):
+        """
+        Apply the model to an input batch.
+        :param x: an [N x C x ...] Tensor of inputs.
+        :param timesteps: a 1-D batch of timesteps.
+        :param context: conditioning plugged in via crossattn
+        :param y: an [N] Tensor of labels, if class-conditional.
+        :return: an [N x C x ...] Tensor of outputs and pooled representations.
+        """
+        assert (y is not None) == (
+            self.num_classes is not None
+        ), "must specify y if and only if the model is class-conditional"
+        hs = []
+        t_emb = timestep_embedding(timesteps, self.model_channels, repeat_only=False)
+        emb = self.time_embed(t_emb)
+
+        if self.num_classes is not None:
+            assert y.shape == (x.shape[0],)
+            emb = emb + self.label_emb(y)
+
+        h = x.type(self.dtype)
+        for module in self.input_blocks:
+            h = module(h, emb, context)
+            hs.append(h)
+        h = self.middle_block(h, emb, context)
+        representations = [th.nn.functional.avg_pool2d(h, 2)]
+        for module in self.output_blocks:
+            last_hs = hs.pop()
+            representations.append(th.nn.functional.avg_pool2d(last_hs, 2))
+            h = th.cat([h, last_hs], dim=1)
+            h = module(h, emb, context)
+        h = h.type(x.dtype)
+        if self.predict_codebook_ids:
+            return self.id_predictor(h)
+        else:
+            return self.out(h), representations
