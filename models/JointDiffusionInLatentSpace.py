86b91f3bb52ca7ffff323b46d78aeec886153571
Pawel Skiers <pawelsk269@gmail.com>
2023-03-29 01:45:08 +0200

    Add joint diffusion model

--- models/JointDiffusionInLatentSpace.py
+++ models/JointDiffusionInLatentSpace.py
@@ -1,11 +1,17 @@
+import torch
+import torch.nn as nn
 from ldm.models.diffusion.ddpm import LatentDiffusion
-
+from ldm.util import default
 
 class JointDiffusionInLatentSpace(LatentDiffusion):
     def __init__(
             self,
             first_stage_config,
             cond_stage_config,
+            classifier_in_features,
+            classifier_hidden,
+            num_classes,
+            classification_key=1,
             num_timesteps_cond=None,
             cond_stage_key="image",
             cond_stage_trainable=False,
@@ -31,10 +37,84 @@ class JointDiffusionInLatentSpace(LatentDiffusion):
             *args,
             **kwargs
         )
+        self.num_classes = num_classes
+        self.classification_key = classification_key
+        self.classifier = nn.Sequential(
+            nn.Linear(classifier_in_features, classifier_hidden),
+            nn.ReLU(),
+            nn.Linear(classifier_hidden, self.num_classes)
+        )
+
+    def shared_step(self, batch, **kwargs):
+        x, c, classes = self.get_input(batch, self.first_stage_key)
+        loss = self(x, c, classes)
+        return loss
+
+    def get_input(self, batch, k, return_first_stage_outputs=False, force_c_encode=False, cond_key=None, return_original_cond=False, bs=None):
+        ldm_input = super().get_input(batch, k, return_first_stage_outputs, force_c_encode, cond_key, return_original_cond, bs)
+        classes = batch[self.classification_key]
+        return *ldm_input, classes
 
     def apply_model(self, x_noisy, t, cond, return_ids=False):
-        return super().apply_model(x_noisy, t, cond, return_ids)
+        if isinstance(cond, dict):
+            # hybrid case, cond is exptected to be a dict
+            pass
+        else:
+            if not isinstance(cond, list):
+                cond = [cond]
+            key = 'c_concat' if self.model.conditioning_key == 'concat' else 'c_crossattn'
+            cond = {key: cond}
 
-    def p_losses(self, x_start, cond, t, noise=None):
-        return super().p_losses(x_start, cond, t, noise)
+        if hasattr(self, "split_input_params"):
+            raise NotImplementedError("This feature is not available for this model")
+
+        x_recon, representations = self.model(x_noisy, t, **cond)
+        representations = [torch.flatten(z_i, start_dim=1) for z_i in representations]
+        representations = torch.concat(representations, dim=1)
+        predictions = self.classifier(representations)
+
+        if isinstance(x_recon, tuple) and not return_ids:
+            return x_recon[0], predictions
+        else:
+            return x_recon, predictions
+
+    def p_losses(self, x_start, cond, t, classes, noise=None):
+        noise = default(noise, lambda: torch.randn_like(x_start))
+        x_noisy = self.q_sample(x_start=x_start, t=t, noise=noise)
+        model_output, predictions = self.apply_model(x_noisy, t, cond)
+
+        loss_dict = {}
+        prefix = 'train' if self.training else 'val'
+
+        if self.parameterization == "x0":
+            target = x_start
+        elif self.parameterization == "eps":
+            target = noise
+        else:
+            raise NotImplementedError()
+
+        loss_simple = self.get_loss(model_output, target, mean=False).mean([1, 2, 3])
+        loss_dict.update({f'{prefix}/loss_simple': loss_simple.mean()})
+
+        t = t.cpu()
+        logvar_t = self.logvar[t].to(self.device)
+        loss = loss_simple / torch.exp(logvar_t) + logvar_t
+        # loss = loss_simple / torch.exp(self.logvar) + self.logvar
+        if self.learn_logvar:
+            loss_dict.update({f'{prefix}/loss_gamma': loss.mean()})
+            loss_dict.update({'logvar': self.logvar.data.mean()})
+
+        loss = self.l_simple_weight * loss.mean()
+
+        loss_vlb = self.get_loss(model_output, target, mean=False).mean(dim=(1, 2, 3))
+        loss_vlb = (self.lvlb_weights[t] * loss_vlb).mean()
+        loss_dict.update({f'{prefix}/loss_vlb': loss_vlb})
+        loss += (self.original_elbo_weight * loss_vlb)
+
+        loss_classification = nn.functional.cross_entropy(predictions, classes)
+        loss += loss_classification
+        loss_dict.update({f'{prefix}/loss_classification': loss_classification})
+        loss_dict.update({f'{prefix}/loss': loss})
+
+        return loss, loss_dict
 
---
+++ train_joint_diffusion.py
@@ -0,0 +1,72 @@
+import tensorflow as tf
+from ldm.models.diffusion.ddpm import LatentDiffusion
+from omegaconf import OmegaConf
+import argparse
+import torch
+import pytorch_lightning as pl
+from models import JointDiffusionInLatentSpace
+from datasets.mnist import AdjustedMNIST
+from os import listdir, path
+import datetime
+from callbacks import ImageLogger, CUDACallback, SetupCallback, FIDScoreLogger
+
+if __name__ == "__main__":
+    config = OmegaConf.load("configs/mnist-joint-diffusion.yaml")
+
+    lightning_config = config.pop("lightning", OmegaConf.create())
+
+    trainer_config = lightning_config.get("trainer", OmegaConf.create())
+    trainer_config["accelerator"] = "ddp"
+    trainer_config["gpus"] = 1
+    trainer_opt = argparse.Namespace(**trainer_config)
+    lightning_config.trainer = trainer_config
+
+    train_ds = AdjustedMNIST(train=True)
+    test_ds = AdjustedMNIST(train=False)
+    train_ds, validation_ds = torch.utils.data.random_split(train_ds, [len(train_ds)-len(test_ds), len(test_ds)])
+
+    train_dl = torch.utils.data.DataLoader(train_ds, batch_size=256, shuffle=True, num_workers=0, drop_last=True)
+    valid_dl = torch.utils.data.DataLoader(validation_ds, batch_size=256, shuffle=False, num_workers=0)
+    test_dl = torch.utils.data.DataLoader(test_ds, batch_size=4, shuffle=False, num_workers=0)
+
+    model = JointDiffusionInLatentSpace(**config.model.get("params", dict()))
+    model.learning_rate = config.model.base_learning_rate
+
+    now = datetime.datetime.now().strftime("%Y-%m-%dT%H-%M-%S")
+    nowname = "JointDiffusion_" + now
+    logdir = path.join("logs", nowname)
+    ckptdir = path.join(logdir, "checkpoints")
+    cfgdir = path.join(logdir, "configs")
+
+    trainer_kwargs = dict()
+
+    trainer_kwargs["logger"] = pl.loggers.WandbLogger(name=nowname, id=nowname)
+
+    # modelcheckpoint - use TrainResult/EvalResult(checkpoint_on=metric) to
+    # specify which metric is used to determine best models
+    default_modelckpt_cfg = {
+        "params": {
+            "dirpath": ckptdir,
+            "filename": "{epoch:06}",
+            "verbose": True,
+            "save_last": True,
+        }
+    }
+    if hasattr(model, "monitor"):
+        print(f"Monitoring {model.monitor} as checkpoint metric.")
+        default_modelckpt_cfg["params"]["monitor"] = model.monitor
+        default_modelckpt_cfg["params"]["save_top_k"] = 1
+
+    trainer_kwargs["checkpoint_callback"] = True
+
+    trainer_kwargs["callbacks"] = [
+        pl.callbacks.ModelCheckpoint(**default_modelckpt_cfg["params"]),
+        SetupCallback(resume=False, now=now, logdir=logdir, ckptdir=ckptdir, cfgdir=cfgdir, config=config, lightning_config=lightning_config),
+        ImageLogger(batch_frequency=2000, max_images=8, clamp=True, increase_log_steps=False, log_images_kwargs={"inpaint": False}),
+        CUDACallback()
+    ]
+
+    trainer = pl.Trainer.from_argparse_args(trainer_opt, **trainer_kwargs)
+    trainer.logdir = logdir
+
+    trainer.fit(model, train_dataloaders=train_dl, val_dataloaders=valid_dl)
