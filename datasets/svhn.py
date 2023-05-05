5ac48457f8ee8ed8fc4e74ca05f3f16606853310
Pawel Skiers <pawelsk269@gmail.com>
2023-05-05 23:39:36 +0200

    Update dataset to support semi-supervised learning

--- datasets/svhn.py
+++ datasets/svhn.py
@@ -3,9 +3,15 @@ import torchvision as tv
 
 
 class AdjustedSVHN(torch.utils.data.Dataset):
-    def __init__(self, train="train") -> None:
+    def __init__(self, train="train", labeled_per_class=None) -> None:
         super().__init__()
         self.dataset = tv.datasets.SVHN(root="./data", split=train, download=True, transform=tv.transforms.ToTensor())
+        if labeled_per_class is not None and train == "train":
+            labeled_samples = {i: 0 for i in range(10)}
+            for i, label in enumerate(self.dataset.labels):
+                labeled_samples[label] += 1
+                if labeled_samples[label] > labeled_per_class:
+                    self.dataset.labels[i] = -1
 
     def __len__(self):
         return len(self.dataset)
