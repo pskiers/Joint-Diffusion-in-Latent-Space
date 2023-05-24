180c6bb4d890a893f7c6a6850f5dcc3e86268428
Pawel Skiers <pawelsk269@gmail.com>
2023-05-24 02:26:04 +0200

    Add joint ldm with attention classifier

--- models/joint_latent_diffusion.py
+++ models/joint_latent_diffusion.py
@@ -1,5 +1,6 @@
 import torch
 from .joint_latent_diffusion_noisy_classifier import JointLatentDiffusionNoisyClassifier
+from .representation_transformer import RepresentationTransformer
 
 
 class JointLatentDiffusion(JointLatentDiffusionNoisyClassifier):
@@ -64,13 +65,60 @@ class JointLatentDiffusion(JointLatentDiffusionNoisyClassifier):
         x_recon = self.model.diffusion_model.just_reconstruction(x_noisy, t)
         if self.x_start is not None:
             representations = self.model.diffusion_model.just_representations(
-                self.x_start, torch.ones(self.x_start.shape[0], device=self.device)
+                self.x_start,
+                torch.ones(self.x_start.shape[0], device=self.device),
+                pooled=False
             )
-            representations = [torch.flatten(z_i, start_dim=1) for z_i in representations]
-            representations = torch.concat(representations, dim=1)
+            representations = self.transform_representations(representations)
             self.batch_class_predictions = self.classifier(representations)
 
         if isinstance(x_recon, tuple) and not return_ids:
             return x_recon[0]
         else:
             return x_recon
+
+
+class JointLatentDiffusionAttention(JointLatentDiffusion):
+    def __init__(
+            self,
+            first_stage_config,
+            cond_stage_config,
+            attention_config,
+            classification_key=1,
+            num_timesteps_cond=None,
+            cond_stage_key="image",
+            cond_stage_trainable=False,
+            concat_mode=True,
+            cond_stage_forward=None,
+            conditioning_key=None,
+            scale_factor=1,
+            scale_by_std=False,
+            *args,
+            **kwargs
+        ):
+        super().__init__(
+            first_stage_config,
+            cond_stage_config,
+            0,
+            0,
+            0,
+            classification_key,
+            num_timesteps_cond,
+            cond_stage_key,
+            cond_stage_trainable,
+            concat_mode,
+            cond_stage_forward,
+            conditioning_key,
+            scale_factor,
+            scale_by_std,
+            *args,
+            **kwargs
+        )
+        self.classifier = RepresentationTransformer(**attention_config)
+        if kwargs.get("ckpt_path", None) is not None:
+            ignore_keys = kwargs.get("ignore_keys", [])
+            only_model = kwargs.get("load_only_unet", False)
+            self.init_from_ckpt(kwargs["ckpt_path"], ignore_keys=ignore_keys, only_model=only_model)
+
+    def transform_representations(self, representations):
+        return representations
