c6212eae339f526866ff72f75537a507881d9420
Pawel Skiers <pawelsk269@gmail.com>
2023-07-16 01:27:45 +0200

    Add ddpm suitable for wide resnet unet

--- models/baselines/__init__.py
+++ models/baselines/__init__.py
@@ -2,3 +2,4 @@ from .attention_on_latent_diffusion import AttentionOnLatentDiffusion
 from .wide_resnet import WideResNet, WideResNetEncoder
 from .classifier_on_latent_diffusion import ClassifierOnLatentDiffusion
 from .fixmatch import FixMatch
+from .ddpm_wideresnet import DDPM_Wide_ResNet
---
+++ models/baselines/ddpm_wideresnet.py
@@ -0,0 +1,53 @@
+import torch
+import torch.nn as nn
+from ldm.util import default
+from ldm.models.diffusion.ddpm import DDPM
+
+
+class DDPM_Wide_ResNet(DDPM):
+    def apply_model(self, x_noisy, t, return_ids=False):
+        if hasattr(self, "split_input_params"):
+            raise NotImplementedError(
+                "This feature is not available for this model")
+
+        x_recon = self.model.diffusion_model.just_reconstruction(x_noisy, t, pooled=False)
+        if isinstance(x_recon, tuple) and not return_ids:
+            return x_recon[0]
+        else:
+            return x_recon
+
+    def p_losses(self, x_start, t, noise=None):
+        noise = default(noise, lambda: torch.randn_like(x_start))
+        x_noisy = self.q_sample(x_start=x_start, t=t, noise=noise)
+        model_output = self.apply_model(x_noisy, t)
+
+        loss_dict = {}
+        prefix = 'train' if self.training else 'val'
+
+        if self.parameterization == "x0":
+            target = x_start
+        elif self.parameterization == "eps":
+            target = noise
+        else:
+            raise NotImplementedError()
+
+        loss_simple = self.get_loss(model_output, target, mean=False).mean([1, 2, 3])
+        loss_dict.update({f'{prefix}/loss_simple': loss_simple.mean()})
+
+        t = t.cpu()
+        logvar_t = self.logvar[t].to(self.device)
+        loss = loss_simple / torch.exp(logvar_t) + logvar_t
+        # loss = loss_simple / torch.exp(self.logvar) + self.logvar
+        if self.learn_logvar:
+            loss_dict.update({f'{prefix}/loss_gamma': loss.mean()})
+            loss_dict.update({'logvar': self.logvar.data.mean()})
+
+        loss = self.l_simple_weight * loss.mean()
+
+        loss_vlb = self.get_loss(model_output, target, mean=False).mean(dim=(1, 2, 3))
+        loss_vlb = (self.lvlb_weights[t] * loss_vlb).mean()
+        loss_dict.update({f'{prefix}/loss_vlb': loss_vlb})
+        loss += (self.original_elbo_weight * loss_vlb)
+        loss_dict.update({f'{prefix}/loss': loss})
+
+        return loss, loss_dict
