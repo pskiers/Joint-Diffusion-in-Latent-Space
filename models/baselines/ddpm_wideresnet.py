87a0e36089fe649dff8941bb2a134552fa9a7813
Pawel Skiers <pawelsk269@gmail.com>
2023-07-16 02:55:17 +0200

    Fix img sampling

--- models/baselines/ddpm_wideresnet.py
+++ models/baselines/ddpm_wideresnet.py
@@ -51,3 +51,15 @@ class DDPM_Wide_ResNet(DDPM):
         loss_dict.update({f'{prefix}/loss': loss})
 
         return loss, loss_dict
+
+    def p_mean_variance(self, x, t, clip_denoised: bool):
+        model_out = self.model(x, t)
+        if self.parameterization == "eps":
+            x_recon = self.predict_start_from_noise(x, t=t, noise=model_out[0])
+        elif self.parameterization == "x0":
+            x_recon = model_out
+        if clip_denoised:
+            x_recon.clamp_(-1., 1.)
+
+        model_mean, posterior_variance, posterior_log_variance = self.q_posterior(x_start=x_recon, x_t=x, t=t)
+        return model_mean, posterior_variance, posterior_log_variance
