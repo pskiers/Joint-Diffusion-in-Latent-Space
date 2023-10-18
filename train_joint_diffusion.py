5452e8a6c201a1b248903d3293abda7297bbf04c
Pawel Skiers <pawelsk269@gmail.com>
2023-10-18 22:40:47 +0200

    Quickfix

--- train_joint_diffusion.py
+++ train_joint_diffusion.py
@@ -90,7 +90,8 @@ if __name__ == "__main__":
     if (img_logger_cfg := callback_cfg.get("img_logger", None)) is not None:
         trainer_kwargs["callbacks"].append(ImageLogger(**img_logger_cfg))
 
-    if (fid_cfg := dict(callback_cfg.get("fid_logger", None))) is not None:
+    if (fid_cfg := callback_cfg.get("fid_logger", None)) is not None:
+        fid_cfg = dict(fid_cfg)
         fid_cfg["real_dl"] = train_dls[1] if type(train_dls) in (tuple, list) else train_dls  # first dataloader should contain the original images
         fid_cfg["device"] = torch.device("cuda")
         trainer_kwargs["callbacks"].append(FIDScoreLogger(**fid_cfg))
