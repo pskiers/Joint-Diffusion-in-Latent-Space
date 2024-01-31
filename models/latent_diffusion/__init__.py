a8c6b935e1a73da01e4c4b8456f75b859c2d6ec7
joaxkal <joannakaletaa@gmail.com>
2024-01-31 10:59:50 +0100

    fix import for joint diffusion multilabel

--- models/latent_diffusion/__init__.py
+++ models/latent_diffusion/__init__.py
@@ -1,4 +1,5 @@
 from .joint_latent_diffusion_noisy_classifier import JointLatentDiffusionNoisyClassifier, JointLatentDiffusionNoisyAttention
 from .joint_latent_diffusion import JointLatentDiffusion, JointLatentDiffusionAttention
+from .joint_latent_diffusion_multilabel import JointLatentDiffusionMultilabel
 from .ssl_joint_diffusion import SSLJointLatentDiffusion, SSLJointLatentDiffusionV2, SSLJointLatentDiffusionV3
 from .diff_match import LatentDiffMatch, LatentDiffMatchV2, LatentDiffMatchV3, LatentDiffMatchWithSampling, LatentDiffMatchPooling, LatentDiffMatchAttention
