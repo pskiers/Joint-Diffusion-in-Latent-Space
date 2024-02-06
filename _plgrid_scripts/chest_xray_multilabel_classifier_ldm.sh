5bc260b38a14bbdb7b76293f15351563f2189d63
joaxkal <joannakaletaa@gmail.com>
2024-02-06 20:21:11 +0100

    update configs

--- _plgrid_scripts/chest_xray_multilabel_classifier_ldm.sh
+++ _plgrid_scripts/chest_xray_multilabel_classifier_ldm.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name=very_strong_aug_enc_pool8_doubdrop30_lr104_noweight_noft
+#SBATCH --job-name=small_aug_enc_pool8_doubdrop30_lr104_noweight_noft_ch256
 #SBATCH --time=48:00:00
 #SBATCH --partition=plgrid-gpu-a100
 #SBATCH --account=plgsano4-gpu-a100
@@ -11,4 +11,4 @@
 
 module load Miniconda3/4.9.2
 nvidia-smi
-srun $SCRATCH/ldm/bin/python train_classifier_on_ldm.py -p configs/baselines/chest_xray_multilabel_classifier_ldm.yaml --prefix very_strong_aug_enc_pool8_doubdrop_lr104_noweight_noft
+srun $SCRATCH/ldm/bin/python train_classifier_on_ldm.py -p configs/baselines/chest_xray_multilabel_classifier_ldm.yaml --prefix small_aug_enc_pool8_doubdrop_lr104_noweight_noft_ch256
--- _plgrid_scripts/chest_xray_multilabel_ldm.sh
+++ _plgrid_scripts/chest_xray_multilabel_ldm.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name=ram_jd_lr105_class102_very_strong_aug_noweight
+#SBATCH --job-name=jd_lr5_5_class102_old_enc_noft_noweight_ch320
 #SBATCH --time=48:00:00
 #SBATCH --partition=plgrid-gpu-a100
 #SBATCH --account=plgsano4-gpu-a100
@@ -11,4 +11,4 @@
 
 module load Miniconda3/4.9.2
 nvidia-smi
-srun $SCRATCH/ldm/bin/python train_joint_diffusion.py -p configs/latent_diffusion/supervised/non_noisy_classifier/joint_diffusion_pooling/chest_xray_multilabel.yaml --prefix ram_jd_lr105_class101_very_strong_aug_noweight
+srun $SCRATCH/ldm/bin/python train_joint_diffusion.py -p configs/latent_diffusion/supervised/non_noisy_classifier/joint_diffusion_pooling/chest_xray_multilabel.yaml --prefix jd_lr5_5_class102_old_enc_noft_noweight_ch320
