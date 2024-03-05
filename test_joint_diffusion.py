835d9d47ad9790e2751f37fe994ae01f8012a0f2
joaxkal <joannakaletaa@gmail.com>
2024-03-06 00:00:33 +0100

    clean test script

--- test_joint_diffusion.py
+++ test_joint_diffusion.py
@@ -9,36 +9,43 @@ from os import path, environ
 from pathlib import Path
 import datetime
 from callbacks import ImageLogger, CUDACallback, SetupCallback, FIDScoreLogger
-
+import glob
 
 if __name__ == "__main__":
     environ["WANDB__SERVICE_WAIT"] = "300"
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("--path", "-p", type=Path, required=True, help="path to config file")
-    parser.add_argument("--checkpoint", "-c", type=Path, required=False, help="path to model checkpoint file")
-    parser.add_argument("--prefix", "-pref", type=str, required=False, help="prefix to experiment")
+    parser.add_argument("--ckpt_folder", "-p", type=Path, required=True, help="path to config file")
+    parser.add_argument("--enc", "-enc", type=str, required=False, help="path to encoder to easy access the model", 
+                        default="/home/jk/Joint-Diffusion-in-Latent-Space/logs/compvis32x32x4_all_randomresizedcrop_Autoencoder_2024-01-17T13-15-27/checkpoints/epoch=000025.ckpt")
     args = parser.parse_args()
-    config_path = str(args.path)
-    checkpoint_path = str(args.checkpoint) if args.checkpoint is not None else None
-
+    checkpoint_folder = str(args.ckpt_folder)
+    config_path = glob.glob(f'{checkpoint_folder}/configs/*project.yaml')[0] 
     config = OmegaConf.load(config_path)
+    ckpt_path = glob.glob(f'{checkpoint_folder}/checkpoints/*last.ckpt')[0] 
+    config.model.params["ckpt_path"] = ckpt_path
+    config.model.params.first_stage_config.params.ckpt_path = args.enc
+        
 
-    lightning_config = config.pop("lightning", OmegaConf.create())
-
-    trainer_config = lightning_config.get("trainer", OmegaConf.create())
+    trainer_config = {"gpus": -1,
+                      "accelerator": "ddp"}
     trainer_opt = argparse.Namespace(**trainer_config)
-    lightning_config.trainer = trainer_config
-
-    dl_config = config.pop("dataloaders")
-    _, test_dl = get_dataloaders(**dl_config)
-
-    if checkpoint_path is not None:
-        config.model.params["ckpt_path"] = checkpoint_path
-
+    
+    dl_config = {"name": "chest_xray_nih_patches",
+                 "train_batches": [None],
+                "val_batch": 16,
+                "num_workers": 6,
+                "pin_memory": True,
+                "persistent_workers": True,
+                "training_platform": "local_sano"}
+    
+    test_dl = get_dataloaders(**dl_config)[1]
     model = get_model_class(config.model.get("model_type"))(**config.model.get("params", dict()))
-
     model.learning_rate = config.model.base_learning_rate
+    model.sampling_method = "unconditional"
+    model.gradient_guided_sampling=False
+    
+    
     try:
         model.classifier_lr = config.model.classifier_lr
         print(['CLASSIFIER HAS SEPARATE LR'])
@@ -46,62 +53,8 @@ if __name__ == "__main__":
         model.classifier_lr=None
         print(['CLASSIFIER HAS THE SAME LR'])
 
-    model.sampling_method = "unconditional"
-    model.gradient_guided_sampling=False
-
-    now = datetime.datetime.now().strftime("%Y-%m-%dT%H-%M-%S")
-    nowname = args.prefix + "_" + model.__class__.__name__ + "_" + now
-    logdir = path.join(config.model.logdir,"logs", nowname)
-    ckptdir = path.join(logdir, "checkpoints")
-    cfgdir = path.join(logdir, "configs")
-
-    trainer_kwargs = dict()
-    tags = [
-        dl_config["name"],
-        "all labels" if dl_config["num_labeled"] is None else f"{dl_config['num_labeled']} per class",
-        config.model.get("model_type")
-    ]
-    #trainer_kwargs["logger"] = pl.loggers.WandbLogger(name=nowname, id=nowname, tags=tags, project = "Joint-Diffusion-in-Latent-Space")
-
-    default_modelckpt_cfg = {
-        "params": {
-            "dirpath": ckptdir,
-            "filename": "{epoch:06}",
-            "verbose": True,
-            "save_last": True,
-        }
-    }
-    if hasattr(model, "monitor"):
-        print(f"Monitoring {model.monitor} as checkpoint metric.")
-        default_modelckpt_cfg["params"]["monitor"] = model.monitor
-        default_modelckpt_cfg["params"]["save_top_k"] = 1
-        default_modelckpt_cfg["params"]["mode"] = 'max' if 'auroc'in model.monitor or 'acc' in model.monitor else 'min'
-
-    callback_cfg = lightning_config.get("callbacks", OmegaConf.create())
-    trainer_kwargs["callbacks"] = [
-        pl.callbacks.ModelCheckpoint(**default_modelckpt_cfg["params"]),
-        SetupCallback(
-            resume=False,
-            now=now,
-            logdir=logdir,
-            ckptdir=ckptdir,
-            cfgdir=cfgdir,
-            config=config,
-            lightning_config=lightning_config
-        ),
-        CUDACallback()
-    ]
-    if (img_logger_cfg := callback_cfg.get("img_logger", None)) is not None:
-        trainer_kwargs["callbacks"].append(ImageLogger(**img_logger_cfg))
-
-    if (fid_cfg := callback_cfg.get("fid_logger", None)) is not None:
-        fid_cfg = dict(fid_cfg)
-        fid_cfg["real_dl"] = train_dls[1] if type(train_dls) in (tuple, list) else train_dls  # first dataloader should contain the original images
-        fid_cfg["device"] = torch.device("cuda")
-        trainer_kwargs["callbacks"].append(FIDScoreLogger(**fid_cfg))
-    print('[WARNING]this scriot  Hardcoded for patches')
-    trainer = pl.Trainer.from_argparse_args(trainer_opt, **trainer_kwargs)
-    trainer.logdir = logdir
+    print('[WARNING]this script  Hardcoded for patches')
+    trainer = pl.Trainer.from_argparse_args(trainer_opt)
 
     trainer.test(
         model,
