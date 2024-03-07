2f8b38bfcd1d5a105452f507473881b66a90ee1e
joaxkal <joannakaletaa@gmail.com>
2024-03-07 19:43:58 +0000

    clean

--- models/latent_diffusion/joint_latent_diffusion_multilabel.py
+++ models/latent_diffusion/joint_latent_diffusion_multilabel.py
@@ -120,7 +120,6 @@ class JointLatentDiffusionMultilabel(JointLatentDiffusionNoisyClassifier):
 
     def train_classification_step(self, batch, loss):
         if self.classification_start > 0:
-            print('HERE CHECK GLOB', self.global_step, self.classification_start)
             self.classification_start -= 1
             return loss
         
