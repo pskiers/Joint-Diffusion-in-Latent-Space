Add different normalization for cl cifar10, loading rehearsed dataset, also samplig fixes

cl_methods/generative_replay.py
@@ -39,6 +39,8 @@ class GenerativeReplay(CLMethod):
         sup_ds: Subset,
         unsup_ds: Subset,
         prev_tasks: List,
+        mean: List[float],
+        std: List[float],
         samples_per_task: int,
         old_sample_generator: Callable[[int, List], Tuple[torch.Tensor, torch.Tensor]],
         new_sample_generator: Optional[
@@ -46,6 +48,8 @@ class GenerativeReplay(CLMethod):
         ] = None,
         current_task: Optional[List] = None,
         filename: str = "",
+        saved_samples: Optional[str] = None,
+        saved_labels: Optional[str] = None,
     ):
         if len(prev_tasks) == 0:
             joined_sup_ds = sup_ds
@@ -55,39 +59,41 @@ class GenerativeReplay(CLMethod):
             to_generate = {task: samples_per_task for task in prev_tasks}
             generated_imgs = torch.tensor([])
             generated_labels = torch.tensor([]).type(torch.LongTensor)
-            for task in to_generate.keys():
-                while to_generate[task] > 0:
-                    bs = min(self.args.sample_batch_size, to_generate[task])
-                    imgs, labels = old_sample_generator(bs, [task for _ in range(bs)])
-                    generated_imgs = torch.concat((generated_imgs, imgs))
-                    generated_labels = torch.concat((generated_labels, labels))
-                    to_generate[task] -= len(labels)
-            if new_sample_generator is not None:
-                to_generate = {task: samples_per_task for task in current_task}
+            if saved_samples is None or saved_labels is None:
                 for task in to_generate.keys():
                     while to_generate[task] > 0:
                         bs = min(self.args.sample_batch_size, to_generate[task])
-                        imgs, labels = new_sample_generator(
+                        imgs, labels = old_sample_generator(
                             bs, [task for _ in range(bs)]
                         )
                         generated_imgs = torch.concat((generated_imgs, imgs))
                         generated_labels = torch.concat((generated_labels, labels))
                         to_generate[task] -= len(labels)
+                if new_sample_generator is not None:
+                    to_generate = {task: samples_per_task for task in current_task}
+                    for task in to_generate.keys():
+                        while to_generate[task] > 0:
+                            bs = min(self.args.sample_batch_size, to_generate[task])
+                            imgs, labels = new_sample_generator(
+                                bs, [task for _ in range(bs)]
+                            )
+                            generated_imgs = torch.concat((generated_imgs, imgs))
+                            generated_labels = torch.concat((generated_labels, labels))
+                            to_generate[task] -= len(labels)
+                torch.save(generated_imgs, f"./data/cl/{filename}_imgs.pt")
+                torch.save(generated_labels, f"./data/cl/{filename}_labels.pt")
+            else:
+                imgs = torch.load(saved_samples)
+                labels = torch.load(saved_labels)
+                generated_imgs = torch.concat((generated_imgs, imgs))
+                generated_labels = torch.concat((generated_labels, labels))
 
-            torch.save(generated_imgs, f"./data/cl/{filename}_imgs.pt")
-            torch.save(generated_labels, f"./data/cl/{filename}_labels.pt")
             # generated_imgs = torch.load("./cifar100_images.pt") if filename == "cifar100_randaugment" else torch.load("./cifar10_images.pt")
             # generated_labels = torch.load("./cifar100_labels.pt").type(torch.LongTensor) if filename == "cifar100_randaugment" else torch.load("./cifar10_labels.pt").type(torch.LongTensor)
 
-            from datasets.fixmatch_cifar import (
-                TransformRandAugmentSupervised,
-                cifar10_mean,
-                cifar10_std,
-            )  # TODO
+            from datasets.fixmatch_cifar import TransformRandAugmentSupervised  # TODO
 
-            transform = TransformRandAugmentSupervised(
-                mean=cifar10_mean, std=cifar10_std
-            )  # TODO
+            transform = TransformRandAugmentSupervised(mean=mean, std=std)  # TODO
 
             gen_sup_ds = DatasetDummy(
                 generated_imgs,

datasets/__init__.py
@@ -1,4 +1,4 @@
-from typing import Tuple, Optional
+from typing import Tuple, Optional, List
 from dataclasses import dataclass
 import torch
 from torch.utils.data import DataLoader, RandomSampler, SequentialSampler, Subset
@@ -295,12 +295,14 @@ def get_cl_datasets(
     name: str,
     num_labeled: int,
     sup_batch: int,
+    mean: Optional[List[float]] = None,
+    std: Optional[List[float]] = None,
 ):
     if name == "cifar10_randaugment":
         if num_labeled is not None:
             args = RandAugmentArgs(num_labeled=num_labeled, num_classes=10, batch_size=sup_batch)
             labeled_dataset, unlabeled_dataset, test_dataset = DATASET_GETTERS["cifar10"](
-                args, "./data", labels_to_tensor=True
+                args, "./data", labels_to_tensor=True, mean=mean, std=std
             )
             tasks = [[0, 1], [2, 3], [4, 5], [6, 7], [8, 9]]
             sup_tasks_indices = cl_class_split(labeled_dataset.targets, tasks)

datasets/fixmatch_cifar.py
@@ -19,19 +19,21 @@ normal_mean = (0.5, 0.5, 0.5)
 normal_std = (0.5, 0.5, 0.5)
 
 
-def get_cifar10(args, root, labels_to_tensor=False):
+def get_cifar10(args, root, labels_to_tensor=False, mean=None, std=None):
+    mean = mean if mean is not None else cifar10_mean
+    std = std if std is not None else cifar10_std
     transform_labeled = transforms.Compose(
         [
             transforms.RandomHorizontalFlip(),
             transforms.RandomCrop(size=32, padding=int(32 * 0.125), padding_mode="reflect"),
             transforms.ToTensor(),
-            transforms.Normalize(mean=cifar10_mean, std=cifar10_std),
+            transforms.Normalize(mean=mean, std=std),
         ]
     )
     transform_val = transforms.Compose(
         [
             transforms.ToTensor(),
-            transforms.Normalize(mean=cifar10_mean, std=cifar10_std),
+            transforms.Normalize(mean=mean, std=std),
         ]
     )
     base_dataset = datasets.CIFAR10(root, train=True, download=True)
@@ -52,7 +54,7 @@ def get_cifar10(args, root, labels_to_tensor=False):
         root,
         train_unlabeled_idxs,
         train=True,
-        transform=TransformFixMatch(mean=cifar10_mean, std=cifar10_std),
+        transform=TransformFixMatch(mean=mean, std=std),
         target_transform=(
             (lambda x: torch.tensor(x).type(torch.LongTensor)) if labels_to_tensor else None
         ),

models/standard_diffusion/joint_diffusion.py
@@ -29,6 +29,7 @@ class JointDiffusionNoisyClassifier(DDPM):
                  classification_key=1,
                  first_stage_key="image",
                  conditioning_key=None,
+                 sampling_method="conditional_to_x",
                  *args,
                  **kwargs):
         super().__init__(
@@ -48,7 +49,7 @@ class JointDiffusionNoisyClassifier(DDPM):
             # nn.ReLU(),
             nn.Linear(classifier_hidden, self.num_classes)
         )
-        self.sampling_method = "conditional_to_x"
+        self.sampling_method = sampling_method
         self.sample_grad_scale = sample_grad_scale
         self.classification_start = classification_start
         self.batch_classes = None
@@ -271,7 +272,6 @@ class JointDiffusion(JointDiffusionNoisyClassifier):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.x_start = None
-        self.sampling_method = "unconditional"
 
     def get_input(self, batch, k):
         out = super().get_input(batch, k)
@@ -368,10 +368,7 @@ class JointDiffusion(JointDiffusionNoisyClassifier):
         if self.sampling_method != "unconditional":
             self.sample_classes = torch.tensor(sample_classes).to(self.device)
         log = dict()
-        if len(batch) == 2:
-            _, _, x, _, _ = self.get_train_input(batch)
-        else:
-            x, _ = self.get_val_input(batch)
+        x = self.get_input(batch, self.first_stage_key)
         N = min(x.shape[0], N)
         n_row = min(x.shape[0], n_row)
         x = x.to(self.device)[:N]

train_joint_diffusion_cl.py
@@ -49,7 +49,7 @@ if __name__ == "__main__":
     )
     args = parser.parse_args()
     config_path = str(args.path)
-    # config_path = "configs/standard_diffusion/continual_learning/diffmatch_pooling/25_per_class/cifar10.yaml"
+    # config_path = "configs/standard_diffusion/continual_learning/joint_diffusion_pooling/cifar10.yaml"
     checkpoint_path = str(args.checkpoint) if args.checkpoint is not None else None
     # checkpoint_path = None
     old_generator_path = str(args.old) if args.old is not None else None
@@ -83,6 +83,8 @@ if __name__ == "__main__":
         name=dl_config["name"],
         num_labeled=dl_config["num_labeled"],
         sup_batch=dl_config["train_batches"][0],
+        mean=dl_config.get("mean", None),
+        std=dl_config.get("std", None),
     )
 
     test_dl = data.DataLoader(
@@ -186,8 +188,15 @@ if __name__ == "__main__":
                 samples = generator.sample(batch_size=batch)
 
                 unet = generator.model.diffusion_model
-                representations = unet.just_representations(samples, torch.zeros_like(generator.sample_classes), context=None, pooled=False)
-                pooled_representations = generator.transform_representations(representations)
+                representations = unet.just_representations(
+                    samples,
+                    torch.zeros_like(generator.sample_classes),
+                    context=None,
+                    pooled=False,
+                )
+                pooled_representations = generator.transform_representations(
+                    representations
+                )
                 pred = generator.classifier(pooled_representations).argmax(dim=-1)
                 ok_class = pred == labels
                 samples = samples[ok_class]
@@ -232,6 +241,8 @@ if __name__ == "__main__":
                 sup_ds=labeled_ds,
                 unsup_ds=unlabeled_ds,
                 prev_tasks=prev_tasks,
+                mean=dl_config["mean"],
+                std=dl_config["std"],
                 samples_per_task=cl_config["samples_per_class"],
                 old_sample_generator=generate_old_samples,
                 new_sample_generator=(
@@ -239,6 +250,8 @@ if __name__ == "__main__":
                 ),  # dummy for class conditioned baseline stuff
                 current_task=task,
                 filename=nowname,
+                saved_samples=cl_config.get("saved_samples", None),
+                saved_labels=cl_config.get("saved_labels", None),
             )
             if new_generator is not None:
                 del new_generator