8eccd06de34e2675399d51b7797475e5f432e060
joaxkal <joannakaletaa@gmail.com>
2024-03-22 10:38:14 +0000

    update make env with note

--- make_env.sh
+++ make_env.sh
@@ -10,3 +10,11 @@ pip install pytorch-fid
 pip install matplotlib
 pip install --upgrade torch torchvision
 pip install git+https://github.com/kornia/kornia
+
+# IMPORTANT NOTE
+# we moved to pytorch-lightning 1.5.0, torchmetrics 0.7.0, 
+# conda install pytorch==2.2.0 torchvision==0.17.0 torchaudio==2.2.0 pytorch-cuda=12.1 -c pytorch -c nvidia
+# transformers 4.6.0
+# packaging 21.3
+# rest of the environment needs to be solved according to current conflicts
+# if problem with sympy, try to reinstall it first, version 1.12 should work fine
